dd2fe32272fccc3aba6d42197b6745cc37fe428b
mrghost143 <ankit1435bisht@gmail.com>
2021-10-25 13:55:17 +0530

    first

---
+++ data.js
@@ -0,0 +1,35 @@
+ export const news=[
+ {
+  "id":1,
+  "day":17,
+  "month":"AUG",
+  "Year":2019,
+  "newsHeading":"Jayakwadi dam’s 16 gates opened, discharge at 35,630 cusec",
+
+"newsDesc":"On Wednesday, four gates of the Jayakwadi dam, one of the biggest dams in the state known as the dbl,ed,cbbluulhkzkbdfaieugv,k"
+
+
+ },
+  {
+  "id":2,
+  "day":17,
+  "month":"AUG",
+  "Year":2019,
+  "newsHeading":"Jayakwadi dam’s 16 gates opened, discharge at 35,630 cusec",
+
+"newsDesc":"On Wednesday, four gates of the Jayakwadi dam, one of the biggest dams in the state known as the dbl,ed,cbbluulhkzkbdfaieugv,k"
+
+
+ },
+  {
+  "id":3,
+  "day":17,
+  "month":"AUG",
+  "Year":2019,
+  "newsHeading":"Jayakwadi dam’s 16 gates opened, discharge at 35,630 cusec",
+
+"newsDesc":"On Wednesday, four gates of the Jayakwadi dam, one of the biggest dams in the state known as the dbl,ed,cbbluulhkzkbdfaieugv,k"
+
+
+ },
+]
---
+++ main.js
@@ -0,0 +1,18 @@
+ import { news } from "./data.js";
+ 
+
+
+
+$(document).ready(function(){
+
+
+
+
+ $(".logo").on("click", function(){
+  $(window).scrollTop(0);
+ })
+})
+
+
+
+
